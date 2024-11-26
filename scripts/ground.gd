f331220c32cf7829639e00236f2d4e2d62c8d0aa
Brandon Gioggia <bgioggia@gmail.com>
2024-11-25 21:44:28 -0500

    triangleStrip version working

--- scripts/ground.gd
+++ scripts/ground.gd
@@ -3,7 +3,8 @@ extends MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var vertices = GenerateFlatLand()
+	#var vertices = GenerateFlatLandPRIMTRI()
+	var vertices = GenerateFlatLandPRIMTRISTRIP()
 	#vertices.push_back(Vector3(1, 0, 1))
 	#vertices.push_back(Vector3(-1, 0, 1))
 	#vertices.push_back(Vector3(1, 0, -1))
@@ -15,13 +16,13 @@ func _ready() -> void:
 	arrays[Mesh.ARRAY_VERTEX] = vertices
 
 	# Create the Mesh.
-	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLE_STRIP, arrays)
 	mesh = arr_mesh
 	
 	
 	
 
-func GenerateFlatLand() -> PackedVector3Array:
+func GenerateFlatLandPRIMTRI() -> PackedVector3Array:
 	#hardcoded flatland that is 400 units in all directions
 	var vertices = PackedVector3Array()
 
@@ -35,6 +36,19 @@ func GenerateFlatLand() -> PackedVector3Array:
 			vertices.push_back(Vector3(x - 0.5, 0, z + 0.5))
 	return vertices
 
+
+func GenerateFlatLandPRIMTRISTRIP() -> PackedVector3Array:
+	#hardcoded flatland that is 400 units in all directions
+	var vertices = PackedVector3Array()
+
+	for x in range(-5, 5):
+		for z in range(-5, 5):
+			vertices.push_back(Vector3(x + 0.5, 0, z - 0.5))
+			vertices.push_back(Vector3(x - 0.5, 0, z - 0.5))
+			vertices.push_back(Vector3(x + 0.5, 0, z + 0.5))
+			vertices.push_back(Vector3(x - 0.5, 0, z + 0.5))
+	return vertices
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
