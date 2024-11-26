3969a56ffcf68fee652c050987d1ccddc198ec13
Brandon Gioggia <bgioggia@gmail.com>
2024-11-25 22:11:18 -0500

    Fixed TRI mesh renderer - TRISTRIP is actually broken

--- scripts/ground.gd
+++ scripts/ground.gd
@@ -3,8 +3,8 @@ extends MeshInstance3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	#var vertices = GenerateFlatLandPRIMTRI()
-	var vertices = GenerateFlatLandPRIMTRISTRIP()
+	var vertices = GenerateFlatLandPRIMTRI()
+	#var vertices = GenerateFlatLandPRIMTRISTRIP()
 	#vertices.push_back(Vector3(1, 0, 1))
 	#vertices.push_back(Vector3(-1, 0, 1))
 	#vertices.push_back(Vector3(1, 0, -1))
@@ -16,8 +16,11 @@ func _ready() -> void:
 	arrays[Mesh.ARRAY_VERTEX] = vertices
 
 	# Create the Mesh.
-	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLE_STRIP, arrays)
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, arrays)
+	#arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLE_STRIP, arrays)
 	mesh = arr_mesh
+	for vertex in vertices:
+		print(vertex)
 	
 	
 	
@@ -25,28 +28,29 @@ func _ready() -> void:
 func GenerateFlatLandPRIMTRI() -> PackedVector3Array:
 	#hardcoded flatland that is 400 units in all directions
 	var vertices = PackedVector3Array()
-
-	for x in range(-5, 5):
-		for z in range(-5, 5):
-			vertices.push_back(Vector3(x + 0.5, 0, z - 0.5))
-			vertices.push_back(Vector3(x - 0.5, 0, z - 0.5))
+	for x in range(-1, 1):
+		for z in range(-1, 1):
 			vertices.push_back(Vector3(x - 0.5, 0, z + 0.5))
 			vertices.push_back(Vector3(x + 0.5, 0, z - 0.5))
 			vertices.push_back(Vector3(x + 0.5, 0, z + 0.5))
+			
 			vertices.push_back(Vector3(x - 0.5, 0, z + 0.5))
+			vertices.push_back(Vector3(x - 0.5, 0, z - 0.5))
+			vertices.push_back(Vector3(x + 0.5, 0, z - 0.5))
+			
 	return vertices
 
 
+#TODO THIS IS STILL BROKEN
 func GenerateFlatLandPRIMTRISTRIP() -> PackedVector3Array:
 	#hardcoded flatland that is 400 units in all directions
 	var vertices = PackedVector3Array()
-
-	for x in range(-5, 5):
-		for z in range(-5, 5):
-			vertices.push_back(Vector3(x + 0.5, 0, z - 0.5))
-			vertices.push_back(Vector3(x - 0.5, 0, z - 0.5))
-			vertices.push_back(Vector3(x + 0.5, 0, z + 0.5))
+	for x in range(-1, 1):
+		for z in range(-1, 1):
 			vertices.push_back(Vector3(x - 0.5, 0, z + 0.5))
+			vertices.push_back(Vector3(x + 0.5, 0, z - 0.5))
+			vertices.push_back(Vector3(x + 0.5, 0, z + 0.5))
+			vertices.push_back(Vector3(x - 0.5, 0, z - 0.5))
 	return vertices
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
